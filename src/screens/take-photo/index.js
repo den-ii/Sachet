f71c2f16f4d3d71134cda28b163cc99cf8fda815
tochibedford <musicofbyte@gmail.com>
2024-02-02 09:47:41 +0100

    fix: context not suportted in KaiOs JUP-190

--- src/screens/take-photo/index.js
+++ src/screens/take-photo/index.js
@@ -1,107 +1,116 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react"
-import Header from "../../components/header"
-import Softkey from "../../components/softkey"
-import "./styles.css"
+import { useState, useEffect } from "react";
+import Header from "../../components/header";
+import Softkey from "../../components/softkey";
+import "./styles.css";
+import { encrypt } from "../../encryption";
 
 function TakePhoto({ next }) {
-  const [capture, setCapture] = useState(false)
-
+  const [capture, setCapture] = useState(false);
+  useEffect(() => {
+    return () => {
+      localStorage.setItem(
+        "phoneNumber",
+        encrypt(JSON.stringify({ phoneNumber: "123456789" }))
+      );
+    };
+  }, []);
 
   function clearphoto() {
-    let canvas = document.getElementById("canvas")
+    let canvas = document.getElementById("canvas");
     // let photo = document.getElementById("photo")
-    let width = screen.width > 600? 350: 225
-    let height = screen.width > 600? 350: 225
+    let width = screen.width > 600 ? 350 : 225;
+    let height = screen.width > 600 ? 350 : 225;
 
-    const context = canvas.getContext("2d")
-    context.fillStyle = "#AAA"
-    context.fillRect(0, 0, width, height)
+    const context = canvas.getContext("2d");
+    context.fillStyle = "#AAA";
+    context.fillRect(0, 0, width, height);
 
-    const data = canvas.toDataURL("image/png")
+    const data = canvas.toDataURL("image/png");
     // photo.setAttribute("src", data)
   }
-  
+
   function takepicture() {
-    document.getElementById("cameraContainer").classList.add("hidden")
-    document.getElementById("previewPhotoContainer").classList.remove("hidden")
-    let canvas = document.getElementById("canvas")
-    let photo = document.getElementById("photo")
-    let video = document.getElementById("video")
-    let width =  screen.width > 600? 350: 225
-    let height = screen.width > 600? 350: 225
+    document.getElementById("cameraContainer").classList.add("hidden");
+    document.getElementById("previewPhotoContainer").classList.remove("hidden");
+    let canvas = document.getElementById("canvas");
+    let photo = document.getElementById("photo");
+    let video = document.getElementById("video");
+    let width = screen.width > 600 ? 350 : 225;
+    let height = screen.width > 600 ? 350 : 225;
 
-    const context = canvas.getContext("2d")
+    const context = canvas.getContext("2d");
     if (width && height) {
-      canvas.width = width
-      canvas.height = height
-      context.drawImage(video, 0, 0, width, height)
+      canvas.width = width;
+      canvas.height = height;
+      context.drawImage(video, 0, 0, width, height);
 
-      const data = canvas.toDataURL("image/png")
+      const data = canvas.toDataURL("image/png");
       // photo.setAttribute("src", data)
     } else {
-      clearphoto()
+      clearphoto();
     }
   }
-  
 
-  useEffect(() => {   
-    let video = document.getElementById("video")
-    let startbutton = document.getElementById("startbutton")
+  useEffect(() => {
+    let video = document.getElementById("video");
+    let startbutton = document.getElementById("startbutton");
 
-    let mediaStream = null
-    let videoTracks = null
+    let mediaStream = null;
+    let videoTracks = null;
 
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-      .then(function(stream) {
-        mediaStream = stream
-        videoTracks = stream.getVideoTracks()
-        
-        video.srcObject = stream
-        video.play()
-      }).catch((err) => {
-        console.error(`An error occurred: ${err}`)
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then(function (stream) {
+        mediaStream = stream;
+        videoTracks = stream.getVideoTracks();
+
+        video.srcObject = stream;
+        video.play();
       })
-      
-    clearphoto()
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+
+    clearphoto();
     return () => {
       // Stop the video tracks
       if (videoTracks) {
-        videoTracks.forEach(track => track.stop());
+        videoTracks.forEach((track) => track.stop());
       }
 
       // Stop the media stream
       if (mediaStream) {
-        mediaStream.getTracks().forEach(track => track.stop());
+        mediaStream.getTracks().forEach((track) => track.stop());
       }
     };
-  }, [])
+  }, []);
 
-  function handlePicture(ev){
-    ev.preventDefault()
-    takepicture()  
+  function handlePicture(ev) {
+    ev.preventDefault();
+    takepicture();
   }
-  
+
   function retakePhoto() {
-    document.getElementById("previewPhotoContainer").classList.add("hidden")
-    document.getElementById("cameraContainer").classList.remove("hidden")
+    document.getElementById("previewPhotoContainer").classList.add("hidden");
+    document.getElementById("cameraContainer").classList.remove("hidden");
   }
-  
-  
+
   return (
     <div className="takephotoContainer">
       <div id="cameraContainer" className="cameraContainer">
         <div className="camera">
           <video id="video">Not available.</video>
           <div className="caption"></div>
-          <div className="advice">Please position your face within the frame.</div>
+          <div className="advice">
+            Please position your face within the frame.
+          </div>
         </div>
         {/* <button id="startbutton"></button> */}
         <Softkey center="Take Photo" onKeyCenter={handlePicture} />
-
       </div>
       <div id="previewPhotoContainer" className="previewPhotoContainer hidden">
-        <Header title="Confirm Picture"/>
+        <Header title="Confirm Picture" />
         <canvas id="canvas" className="photo"></canvas>
         <div className="advice">
           Make sure your face is not blur or out of frame before continuing
@@ -109,11 +118,15 @@ function TakePhoto({ next }) {
         {/* <div className="output">
           <img id="photo"className="photo" alt="The screen capture will appear in this box."/>
         </div> */}
-        <Softkey left="Retake Photo" onKeyLeft={retakePhoto} right="Verify" onKeyRight={next} />
-
+        <Softkey
+          left="Retake Photo"
+          onKeyLeft={retakePhoto}
+          right="Verify"
+          onKeyRight={next}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default TakePhoto
+export default TakePhoto;
