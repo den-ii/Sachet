3fd7a4a8aae343891b179cde80cf5a6005343db5
juicio8 <wisdomochiche@gmail.com>
2024-05-24 20:04:08 +0100

    fix url

--- src/screens/password-setup/index.js
+++ src/screens/password-setup/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Header from "../../components/header";
 import Softkey from "../../components/softkey";
 import DotsLoader from "../../components/dots-loader";
