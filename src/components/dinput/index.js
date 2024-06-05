3693db6332e4d4f788d5ea2a3713a859e7a5840e
den-ii <wisdomochiche@gmail.com>
2024-06-05 23:25:24 +0100

    fix: rest parameter broke app

--- src/components/dinput/index.js
+++ src/components/dinput/index.js
@@ -1,9 +1,9 @@
 import "./styles.css";
 
-function Dinput({ id, type, value, iRef, ...props }) {
+function Dinput({ id, type, value, iRef }) {
   return (
     <div className="Dinput">
-      <input id={id} type={type} value={value} ref={iRef} {...props} />
+      <input id={id} type={type} value={value} ref={iRef} />
     </div>
   );
 }
